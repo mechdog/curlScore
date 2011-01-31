e0637b31414b3769a6f1e3353a25f540b5ea1b07
Mark Kenney <mkenney@mark-kenneys-imac.local>
2011-01-30 23:24:06 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CurlScore::Application.config.secret_token = '5f3186c268643c1340dedbedf8bd96702702065849a19ca9aebaca259c5b67b8a7a5f148f818e93b59c3a9361be4aab3b2982c8191d20e4a4d0e75b8110a14d0'
